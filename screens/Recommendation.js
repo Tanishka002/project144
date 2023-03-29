5a64c2339d9c6f7a06053c4661e3911607c6ea8e
Tanishka002 <75033184+Tanishka002@users.noreply.github.com>
2023-03-29 16:30:59 +0300

    Add files via upload

---
+++ screens/Recommendation.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  ImageBackground
+} from "react-native";
+import axios from "axios";
+import { RFValue } from "react-native-responsive-fontsize";
+import Star from "react-native-star-view";
+
+export default class PopularScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      ngrok_url:""
+    };
+  }
+
+  getData = () => {
+    /*Complete the getData() function.*/
+    const link = this.state.url+"/recommened_movies";
+    axios.get(link).then( async (response)=>{
+      this.setState({data: response.data.data})
+    }).catch((error)=>{console.log(error)})
+
+
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/bg.png")}
+          style={{ flex: 1 }}
+        >
+          {/* Add FlatList component here to show the list of articles*/}
+          <Flatlist 
+           data={this.state.data}
+           renderItem={this.display}
+           keyExtractor={(item, index)=>{index.string()}}
+          />
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  cardContainer: {
+    width: RFValue(280),
+    alignSelf: "center",
+    backgroundColor: "white",
+    borderRadius: RFValue(10),
+    margin: RFValue(10),
+    padding: RFValue(10),
+    borderColor: "#182854",
+    borderWidth: RFValue(2),
+  },
+  posterImage: {
+    flex: 1,
+    borderRadius: RFValue(10),
+  },
+  title: {
+    fontSize: RFValue(15),
+    fontWeight: "bold",
+    color: "#182854",
+    fontFamily: "monospace",
+    marginVertical: RFValue(5),
+  },
+  subtitle: {
+    fontSize: RFValue(10),
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "monospace",
+    marginVertical: RFValue(2),
+  },
+  movieTitleContainer: {
+    position: "absolute",
+    backgroundColor: "#3c8ed9",
+    opacity: 0.7,
+    padding: RFValue(10),
+    bottom: RFValue(10),
+    left: RFValue(10),
+    borderRadius: RFValue(10),
+  },
+  starStyle: {
+    width: RFValue(200),
+    height: RFValue(40),
+  },
+});
